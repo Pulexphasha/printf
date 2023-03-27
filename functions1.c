6484f8779c2a48dd152c9f586e0ed32147b4806f
Pulexphasha <puliephasha@gmail.com>
2023-03-27 08:32:40 -0700

    printf

--- functions1.c
+++ functions1.c
@@ -11,7 +11,6 @@
  * @size: Size specifier
  * Return: Number of chars printed.
  */
-
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -47,7 +46,6 @@ int print_unsigned(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -90,7 +88,6 @@ int print_octal(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -109,7 +106,6 @@ int print_hexadecimal(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
-
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -131,7 +127,6 @@ int print_hexa_upper(va_list types, char buffer[],
  * @size: Size specification
  * Return: Number of chars printed
  */
-
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
